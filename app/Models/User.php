6d97f3d19897231024fbe882e8225ebee00463b3
Shakhzod <93878055+Shakhzod0307@users.noreply.github.com>
2024-04-16 22:09:26 +0500

    application model added

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,6 +39,7 @@ class User extends Authenticatable
     }
 
     public function role(){return $this->belongsTo(Role::class);}
+    public function applications(){return $this->hasMany(Application::class);}
 
 
 }
