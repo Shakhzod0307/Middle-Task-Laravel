41b7a5cb6d3a7cb112ef675b26aa82d871b570b2
Shakhzod <93878055+Shakhzod0307@users.noreply.github.com>
2024-04-18 17:12:36 +0500

    adding file icon

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,16 +24,45 @@
                                         <div class="text-xs text-neutral-500">{{$application->created_at}}</div>
                                     </div>
                                 </div>
+                                <div class="flex justify-between">
+                                    <div>
+                                        <div class="mt-4 mb-6">
+                                            <div class="mb-3 text-xl font-bold">{{$application->subject}}</div>
+                                            <div class="text-sm text-neutral-600">{{$application->message}}</div>
+                                        </div>
 
-                                <div class="mt-4 mb-6">
-                                    <div class="mb-3 text-xl font-bold">{{$application->subject}}</div>
-                                    <div class="text-sm text-neutral-600">{{$application->message}}</div>
-                                </div>
-
-                                <div>
-                                    <div class="flex items-center justify-between text-slate-500">
-                                        {{$application->user->email}}
+                                        <div>
+                                            <div class="flex items-center justify-between text-slate-500">
+                                                {{$application->user->email}}
+                                            </div>
+                                        </div>
                                     </div>
+                                    @if($application->file_url)
+                                    <div class="items-center p-10 mt-3 border-2 hover:bg-gray-100 transition">
+                                        <a  href="{{asset('storage/'.$application->file_url)}}" target="_blank">
+                                            <svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                             width="30px" height="30px" viewBox="0 0 61.994 61.994"
+                                             xml:space="preserve">
+                                                <g>
+                                                    <g>
+                                                        <path d="M54.988,23.789l-0.031-0.113c-0.025-0.193-0.07-0.38-0.135-0.572l-0.064-0.192l-0.046-0.099
+                                                            c-0.103-0.222-0.222-0.418-0.358-0.597l-0.033-0.068L33.05,0.884c-0.248-0.248-0.537-0.446-0.9-0.613l-0.253-0.094
+                                                            c-0.187-0.064-0.378-0.109-0.581-0.137L31.169,0H17C11.486,0,7,4.485,7,10v41.994c0,5.514,4.486,10,10.001,10h27.996
+                                                            c5.515,0,9.998-4.484,9.998-10V24.073L54.988,23.789z M48.994,51.993c0,2.203-1.793,3.997-3.997,3.997H17
+                                                            c-2.205,0-3.999-1.794-3.999-3.997V9.999C13.001,7.794,14.794,6,17,6h10.921v11.072c0,5.514,4.486,10.001,10,10.001h11.072V51.993
+                                                            z"/>
+                                                        <path d="M14.675,34.112c0,1.291,1.046,2.337,2.337,2.337h26.487c1.291,0,2.337-1.047,2.337-2.337s-1.046-2.337-2.337-2.337H17.012
+                                                            C15.721,31.775,14.675,32.822,14.675,34.112z"/>
+                                                        <path d="M43.499,38.008H17.012c-1.291,0-2.337,1.047-2.337,2.336c0,1.291,1.046,2.338,2.337,2.338h26.487
+                                                            c1.291,0,2.337-1.047,2.337-2.338C45.836,39.055,44.79,38.008,43.499,38.008z"/>
+                                                        <path d="M43.499,44.24H17.012c-1.291,0-2.337,1.046-2.337,2.337s1.046,2.337,2.337,2.337h26.487c1.291,0,2.337-1.047,2.337-2.337
+                                                            S44.79,44.24,43.499,44.24z"/>
+                                                    </g>
+                                                </g>
+                                        </svg>
+                                        </a>
+                                    </div>
+                                    @endif
                                 </div>
                             </div>
                         </div>
